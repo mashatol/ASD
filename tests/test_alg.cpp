187f3a98e6331784899fd1234f058a9d6214db91
Mary <mashatolstogyzowa@gmail.com>
2025-11-11 21:53:27 +0300

    added realizations of check brackets for stack with tests

---
+++ tests/test_alg.cpp
@@ -0,0 +1,97 @@
+#include <gtest/gtest.h>
+#include "alg.h"
+
+TEST(BracketsTest, EmptyString) {
+    EXPECT_TRUE(check_brackets(""));
+}
+
+TEST(BracketsTest, SinglePairs) {
+    EXPECT_TRUE(check_brackets("()"));
+    EXPECT_TRUE(check_brackets("[]"));
+    EXPECT_TRUE(check_brackets("{}"));
+}
+
+TEST(BracketsTest, MultiplePairs) {
+    EXPECT_TRUE(check_brackets("()[]{}"));
+    EXPECT_TRUE(check_brackets("([])"));
+    EXPECT_TRUE(check_brackets("{()}"));
+    EXPECT_TRUE(check_brackets("[{}]"));
+}
+
+TEST(BracketsTest, NestedBrackets) {
+    EXPECT_TRUE(check_brackets("({[]})"));
+    EXPECT_TRUE(check_brackets("((()))"));
+    EXPECT_TRUE(check_brackets("{{{}}}"));
+    EXPECT_TRUE(check_brackets("[[[]]]"));
+    EXPECT_TRUE(check_brackets("({[{}]})"));
+}
+
+TEST(BracketsTest, ComplexCorrectSequences) {
+    EXPECT_TRUE(check_brackets("()()()()"));
+    EXPECT_TRUE(check_brackets("(([]{}))"));
+    EXPECT_TRUE(check_brackets("{()}[{}]"));
+    EXPECT_TRUE(check_brackets("([{()()}])"));
+    EXPECT_TRUE(check_brackets("((((()))))"));
+}
+
+TEST(BracketsTest, WithOtherCharacters) {
+    EXPECT_TRUE(check_brackets("abc(def[ghi]jkl)mno"));
+    EXPECT_TRUE(check_brackets("a + (b * [c - d])"));
+    EXPECT_TRUE(check_brackets("if (x > 0) { y = [1, 2, 3]; }"));
+    EXPECT_TRUE(check_brackets("function(a, b, c) { return a + b; }"));
+    EXPECT_TRUE(check_brackets("array[[index]] = value;"));
+}
+
+TEST(BracketsTest, SingleUnclosed) {
+    EXPECT_FALSE(check_brackets("("));
+    EXPECT_FALSE(check_brackets("["));
+    EXPECT_FALSE(check_brackets("{"));
+    EXPECT_FALSE(check_brackets(")"));
+    EXPECT_FALSE(check_brackets("]"));
+    EXPECT_FALSE(check_brackets("}"));
+}
+
+TEST(BracketsTest, WrongOrder) {
+    EXPECT_FALSE(check_brackets(")("));
+    EXPECT_FALSE(check_brackets("]["));
+    EXPECT_FALSE(check_brackets("}{"));
+    EXPECT_FALSE(check_brackets("([)]"));
+    EXPECT_FALSE(check_brackets("{[}]"));
+    EXPECT_FALSE(check_brackets("(]"));
+    EXPECT_FALSE(check_brackets("[)"));
+    EXPECT_FALSE(check_brackets("{]"));
+}
+
+TEST(BracketsTest, MixedErrors) {
+    EXPECT_FALSE(check_brackets("())"));
+    EXPECT_FALSE(check_brackets("(()"));
+    EXPECT_FALSE(check_brackets("{[}"));
+    EXPECT_FALSE(check_brackets("{{}"));
+    EXPECT_FALSE(check_brackets("[[]"));
+    EXPECT_FALSE(check_brackets("())("));
+    EXPECT_FALSE(check_brackets("{[}]"));
+}
+
+TEST(BracketsTest, OnlyOpening) {
+    EXPECT_FALSE(check_brackets("(((("));
+    EXPECT_FALSE(check_brackets("[[[["));
+    EXPECT_FALSE(check_brackets("{{{{"));
+    EXPECT_FALSE(check_brackets("(([[{{"));
+}
+
+TEST(BracketsTest, OnlyClosing) {
+    EXPECT_FALSE(check_brackets("))))"));
+    EXPECT_FALSE(check_brackets("]]]]"));
+    EXPECT_FALSE(check_brackets("}}}}"));
+    EXPECT_FALSE(check_brackets("))]]}}"));
+}
+
+TEST(BracketsTest, RealWorldExamples) {
+    EXPECT_TRUE(check_brackets("(1 + [2 * {3 - 4}])"));
+    EXPECT_TRUE(check_brackets("array = [1, 2, {key: value}]"));
+    EXPECT_FALSE(check_brackets("if (x > 0 { y = 1; }"));
+    EXPECT_FALSE(check_brackets("function(a, b] { return a + b; }"));
+    EXPECT_FALSE(check_brackets("items = [1, 2, 3));"));
+}
+
+
